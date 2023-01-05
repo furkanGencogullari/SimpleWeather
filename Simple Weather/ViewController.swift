f17d93d74f844236d632adf5d28a4b0bb43f8a63
furkanGencogullari <102527257+furkanGencogullari@users.noreply.github.com>
2023-01-05 18:13:14 +0300

    Initial Commit

---
+++ Simple Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Weather
+//
+//  Created by Furkan Gençoğulları on 5.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
